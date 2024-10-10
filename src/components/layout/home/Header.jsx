c4cf020daf674582a0fa8afddd18f603bb67730f
nuex001 <edeemmanuelchizurumoke@gmail.com>
2024-10-10 18:42:55 +0100

    changed the bg image link from css to our frontend

--- src/components/layout/home/Header.jsx
+++ src/components/layout/home/Header.jsx
@@ -1,34 +1,30 @@
 import React from "react";
-import {
-    ConnectButton,
-  } from "thirdweb/react";
-  import { createWallet, walletConnect } from "thirdweb/wallets";
-  import { ethers } from "ethers";
-  import { Link } from "react-router-dom";
-  import { chain, client } from "../../../utils/Middleware";
+import { ConnectButton } from "thirdweb/react";
+import { createWallet, walletConnect } from "thirdweb/wallets";
+import { ethers } from "ethers";
+import { Link } from "react-router-dom";
+import { chain, client } from "../../../utils/Middleware";
 function Header() {
   const wallets = [createWallet("com.coinbase.wallet")];
   return (
-    <header>
+    <header style={{ backgroundImage: ' url("./basebg.jpg")' }}>
       {/* <img src={bg} alt="" /> */}
       <div className="text">
         <h1>Get Base Testnet Tokens</h1>
         <p>
           Request testnet Base tokens for the Base testnet and test Your Dapps
         </p>
-        <p>
-          Also contribute and Drive points for future Dapps
-        </p>
+        <p>Also contribute and Drive points for future Dapps</p>
       </div>
       <ConnectButton
-          client={client}
-          chain={chain}
-          wallets={wallets}
-          connectButton={{
-            label: "Coinbase Smart Wallet",
-          }}
-          connectModal={{ size: "compact" }}
-        />
+        client={client}
+        chain={chain}
+        wallets={wallets}
+        connectButton={{
+          label: "Coinbase Smart Wallet",
+        }}
+        connectModal={{ size: "compact" }}
+      />
     </header>
   );
 }
